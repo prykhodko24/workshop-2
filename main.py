1f0e4fdc9d67b42b9cfee0d92c54b194a0522e67
tanya-24-01-2001 <44785259+tanya-24-01-2001@users.noreply.github.com>
2020-04-20 08:49:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,17 @@ GROUP BY country
 ORDER BY count(store_number) desc
 fetch first 3 rows only '''
 cursor.execute(query1)
-for row in cursor:
-    print(row)
+proc = list()
+ownersh = list()
+
+num= list()
+country = list()
+
+for numb, ownership_type in cursor:
+    num.append(numb)
+    country.append(country)
+print(num)
+print(country)
 print('\n')
 
 
@@ -21,15 +30,27 @@ query2 = '''SELECT COUNT(store_number) AS procent,ownership_type
 FROM stores
 group by ownership_type '''
 cursor.execute(query2)
-for row in cursor:
-    print(row)
+proc = list()
+ownersh = list()
+
+for procent, ownership_type in cursor:
+    proc.append(procent)
+    ownersh.append(ownership_type)
+print(proc)
+print(ownersh)
 print('\n')
 
 query3 = '''SELECT longitude,latitude
 FROM Stores
 WHERE brand_name='Teavana' '''
 cursor.execute(query3)
-for row in cursor:
-    print(row)
+lati= list()
+longit = list()
+
+for longitude, latitude in cursor:
+    longit.append(longitude)
+    lati.append(latitude)
+print(lati)
+print(longit)
 cursor.close()
 connection.close()

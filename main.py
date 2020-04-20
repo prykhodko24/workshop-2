a2a94c39b0d71fd7459df02044732073ed3ba3e7
tanya-24-01-2001 <44785259+tanya-24-01-2001@users.noreply.github.com>
2020-04-20 08:25:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,29 +6,29 @@ connection = cx_Oracle.connect(username,password, databaseName)
 cursor = connection.cursor()
 
 
-query = '''SELECT COUNT(store_number),country
+query1 = '''SELECT COUNT(store_number),country
 FROM Stores
 GROUP BY country
 ORDER BY count(store_number) desc
 fetch first 3 rows only '''
-cursor.execute(query)
+cursor.execute(query1)
 for row in cursor:
     print(row)
 print('\n')
 
 
-query = '''SELECT COUNT(store_number) AS procent,ownership_type
+query2 = '''SELECT COUNT(store_number) AS procent,ownership_type
 FROM stores
 group by ownership_type '''
-cursor.execute(query)
+cursor.execute(query2)
 for row in cursor:
     print(row)
 print('\n')
 
-query = '''SELECT longitude,latitude
+query3 = '''SELECT longitude,latitude
 FROM Stores
 WHERE brand_name='Teavana' '''
-cursor.execute(query)
+cursor.execute(query3)
 for row in cursor:
     print(row)
 cursor.close()

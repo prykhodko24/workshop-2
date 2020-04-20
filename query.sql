5c1b997e27ca010015da190cbd0edfefd93973ed
tanya-24-01-2001 <44785259+tanya-24-01-2001@users.noreply.github.com>
2020-04-20 11:46:15 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,15 +1,18 @@
 --query1
-select count(store_number),country
-from Stores
-group by country
-order by count(store_number) desc
-fetch first 3 rows only;
+select uu.country, uu.cnt
+from(
+    SELECT country ,count(*) as cnt
+    FROM Stores
+    GROUP BY country
+    ORDER BY count(*) desc
+) uu
+where rownum < 3;
 
 --query2
 select count(store_number) AS procent,ownership_type
 from stores
 group by ownership_type;
 --query3
-SELECT longitude,latitude
+SELECT brand_name, COUNT(*)
 FROM Stores
-WHERE brand_name='Teavana';
+GROUP BY brand_name;
